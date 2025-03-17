c2d012d0db5b49acc436cfde0c27f2ff21be53b3
seom9 <dmssid78@hanmail.net>
2025-03-17 15:45:56 +0900

    20250317 step 시간복잡도

---
+++ src/BEAKJOON/step/시간복잡도/알고리즘의수행시간1.java
@@ -0,0 +1,11 @@
+package BEAKJOON.step.시간복잡도;
+
+public class 알고리즘의수행시간1 {
+    public static void main(String[] args) {
+
+        System.out.println(1);
+        System.out.println(0);
+
+    }
+
+}
---
+++ src/BEAKJOON/step/시간복잡도/알고리즘의수행시간2.java
@@ -0,0 +1,23 @@
+package BEAKJOON.step.시간복잡도;
+
+import java.util.Scanner;
+
+public class 알고리즘의수행시간2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(n);
+        System.out.println(1);
+    }
+
+    // 새로 배운 것 : 의사코드
+    // 프로그램을 작성할 때 각 모듈이 작동하는 논리를 표현하기 위한 언어
+    // 특정 프로그래밍 언어의 문법에 따라 쓰인 것이 아니라,
+    // 일반적인 언어로 코드를 흉내내어 알고리즘을 써놓은 코드를 말한다.
+
+    public int menOfPassion(int n) {
+
+        return 0;
+    }
+}
