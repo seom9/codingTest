777849ed64cc5114ec5fcc5fc9ec36780e75aee2
me1taphor1 <me1taphor1@gmail.com>
2026-02-27 12:43:55 +0900

    20260227

--- src/BEAKJOON/step/정렬/대표값2.java
+++ src/BEAKJOON/step/정렬/대표값2.java
@@ -2,11 +2,23 @@ package BEAKJOON.step.정렬;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class 대표값2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int[] arr = new int[5];
+        int sum = 0;
 
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(sum / 5);
+        System.out.println(arr[2]);
     }
 }
